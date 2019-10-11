65658aec160fd3918888c8ca7443145030796ad3
Brian <balevine@gmail.com>
2019-10-11 20:34:13 +0200

    endpoint should be secret

--- server.js
+++ server.js
@@ -2,31 +2,32 @@
 // where your node app starts
 
 // init project
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
+require('dotenv').config()
 
 // we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-// we've started you off with Express, 
+// we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
-var ideaBot = require('./ideaBot');
+var ideaBot = require('./ideaBot')
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+  response.sendFile(__dirname + '/views/index.html')
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+  console.log('Your app is listening on port ' + listener.address().port)
+})
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.post('/post-a-new-idea-G53x6P5g', function(req, res) {
-  ideaBot.newIdea(req, res);
-});
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.post(process.env.APP_ENDPOINT, function(req, res) {
+  ideaBot.newIdea(req, res)
+})
