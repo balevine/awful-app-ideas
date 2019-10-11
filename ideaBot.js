082cc3df012d1a2330dfe1c6dc2dd987bf2b3b17
Brian <balevine@gmail.com>
2019-10-11 13:04:52 +0200

    try chunked media

--- ideaBot.js
+++ ideaBot.js
@@ -78,10 +78,24 @@ exports.newIdea = (req, res) => {
       access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
     })
 
-    let imageID = await uploadImage(client, image)
+    // let imageID = await uploadImage(client, image)
 
     try {
-      return await client.post('statuses/update', { media_ids: imageID })
+      // const media_id = client.post('media/upload', { media: image })
+      // return media_id
+      initUpload() // Declare that you wish to upload some media
+        .then(appendUpload) // Send the data for the media
+        .then(finalizeUpload) // Declare that you are done uploading chunks
+        .then(mediaId => {
+          // You now have an uploaded movie/animated gif
+          // that you can reference in Tweets, e.g. `update/statuses`
+          // will take a `mediaIds` param.
+          const status = client.post('statuses/update', { media_ids: mediaId })
+        })
+        .then(status => {
+          console.log(status)
+          return status
+        })
     } catch (err) {
       console.log(err)
       return false
@@ -90,11 +104,75 @@ exports.newIdea = (req, res) => {
 
   async function uploadImage(client, image) {
     try {
-      const media_id = client.post('media/upload', { media: image })
-      return media_id
+      // const media_id = client.post('media/upload', { media: image })
+      // return media_id
+      initUpload() // Declare that you wish to upload some media
+        .then(appendUpload) // Send the data for the media
+        .then(finalizeUpload) // Declare that you are done uploading chunks
+        .then(mediaId => {
+          // You now have an uploaded movie/animated gif
+          // that you can reference in Tweets, e.g. `update/statuses`
+          // will take a `mediaIds` param.
+        })
     } catch (err) {
       console.log(err)
       return false
     }
+  }
+
+  /**
+   * Step 1 of 3: Initialize a media upload
+   * @return Promise resolving to String mediaId
+   */
+  function initUpload() {
+    return makePost('media/upload', {
+      command: 'INIT',
+      total_bytes: mediaSize,
+      media_type: mediaType
+    }).then(data => data.media_id_string)
+  }
+
+  /**
+   * Step 2 of 3: Append file chunk
+   * @param String mediaId    Reference to media object being uploaded
+   * @return Promise resolving to String mediaId (for chaining)
+   */
+  function appendUpload(mediaId) {
+    return makePost('media/upload', {
+      command: 'APPEND',
+      media_id: mediaId,
+      media: mediaData,
+      segment_index: 0
+    }).then(data => mediaId)
+  }
+
+  /**
+   * Step 3 of 3: Finalize upload
+   * @param String mediaId   Reference to media
+   * @return Promise resolving to mediaId (for chaining)
+   */
+  function finalizeUpload(mediaId) {
+    return makePost('media/upload', {
+      command: 'FINALIZE',
+      media_id: mediaId
+    }).then(data => mediaId)
+  }
+
+  /**
+   * (Utility function) Send a POST request to the Twitter API
+   * @param String endpoint  e.g. 'statuses/upload'
+   * @param Object params    Params object to send
+   * @return Promise         Rejects if response is error
+   */
+  function makePost(endpoint, params) {
+    return new Promise((resolve, reject) => {
+      client.post(endpoint, params, (error, data, response) => {
+        if (error) {
+          reject(error)
+        } else {
+          resolve(data)
+        }
+      })
+    })
   }
 }
